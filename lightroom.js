2b6bf018d1c093209456c9814af0118099e7283b
icanspeakwhale <icanspeakwhale@gmail.com>
2026-02-10 10:27:25 -0800

    Update lightroom.js

--- lightroom.js
+++ lightroom.js
@@ -1,7 +1,8 @@
-#!name=LightRoom
-#!desc=Adobe LightRoomUnlocked
-#!icon=https://raw.githubusercontent.com/deezertidal/private/main/icons/lightroom.png
-#!homepage=https://yfamilys.com
-#!author=I-am-R-E[MITM]hostname = %APPEND% photos.adobe.io
-[ Script ] 
-LightRoom=type=http-response,pattern=^https:\/\/photos\.adobe\.io\/v2\/accounts*,requires-body=1,script-path=https://raw.githubusercontent.com/litieyin/AD_VIP/main/Script/lightroom.js 
+#! name = LightRoom 
+#! desc  =  Adobe   LightRoomUnlocked  
+#!  icon   =   https   :   //raw.githubusercontent.com/deezertidal/private/main/icons/lightroom.png    
+#!    homepage    =     https     :     //yfamilys.com     
+#!      author      =      I      -      am      -      R      -     E   
+  [     MITM     ]     hostname       = %   APPEND    %     photos    .    adobe    .     io     
+[       Script      ]      
+LightRoom     =     type      =    http     -     response     ,     pattern      =^      https      :\      /\/photos\.adobe\.io\/v2\/accounts*,requires-body=1,script-path=https:/      /      raw      .      githubusercontent      .      com      /      litieyin      /      AD_VIP      /      main      /      Script      /      lightroom      .      js       
